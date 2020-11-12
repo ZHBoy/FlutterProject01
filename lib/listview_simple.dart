051408007467865416fbb12079a4cbd8422bd30f
zhouh <zhouhao@zmeng123.com>
2020-11-12 16:43:33 +0800

    listview组件简单使用

---
+++ lib/listview_simple.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+/*
+ * 顶部导航条，中间内容区域展示demo
+ */
+void main() {
+  runApp(MyApp());
+}
+
+//自定组件就是类
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("列表组件"),
+        ),
+        body: HomeContent3(),
+      ),
+      theme: ThemeData(primarySwatch: Colors.red),
+    );
+  }
+}
+
+//listview中使用ListTile
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          leading: Icon(
+            Icons.search,
+            color: Colors.red,
+            size: 30.0,
+          ),
+          title: Text(
+            '一级标题',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          subtitle: Text('二级标题'),
+        ),
+        ListTile(
+          title: Text('一级标题'),
+          subtitle: Text('二级标题'),
+          trailing: Icon(
+            Icons.deck_rounded,
+            color: Colors.red,
+          ),
+        ),
+        ListTile(
+          leading: Image.network(
+              'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1605168143&di=e92c5258dc948d103b1c85d53fe4f7e4&src=http://a0.att.hudong.com/30/29/01300000201438121627296084016.jpg'),
+          title: Text('一级标题'),
+          subtitle: Text('二级标题'),
+        ),
+        ListTile(
+          title: Text('一级标题'),
+          subtitle: Text('二级标题'),
+        ),
+        ListTile(
+          title: Text('一级标题'),
+          subtitle: Text('二级标题'),
+        ),
+        ListTile(
+          title: Text('一级标题'),
+          subtitle: Text('二级标题'),
+        ),
+        ListTile(
+          title: Text('一级标题'),
+          subtitle: Text('二级标题'),
+        ),
+        ListTile(
+          title: Text('一级标题'),
+          subtitle: Text('二级标题'),
+        )
+      ],
+    );
+  }
+}
+
+//listview中使用其他的widgets
+class HomeContent2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(10.0),
+      children: [
+        Image.network(
+            'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1605168143&di=e92c5258dc948d103b1c85d53fe4f7e4&src=http://a0.att.hudong.com/30/29/01300000201438121627296084016.jpg'),
+        Image.network(
+            'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1605168143&di=e92c5258dc948d103b1c85d53fe4f7e4&src=http://a0.att.hudong.com/30/29/01300000201438121627296084016.jpg'),
+        Image.network(
+            'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1605168143&di=e92c5258dc948d103b1c85d53fe4f7e4&src=http://a0.att.hudong.com/30/29/01300000201438121627296084016.jpg'),
+        Image.network(
+            'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1605168143&di=e92c5258dc948d103b1c85d53fe4f7e4&src=http://a0.att.hudong.com/30/29/01300000201438121627296084016.jpg'),
+        Image.network(
+            'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1605168143&di=e92c5258dc948d103b1c85d53fe4f7e4&src=http://a0.att.hudong.com/30/29/01300000201438121627296084016.jpg'),
+      ],
+    );
+  }
+}
+
+//listview水平列表
+class HomeContent3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Container(
+            width: 180.0,
+            height: 100.0,
+            color: Colors.red,
+          ),
+          Container(
+            width: 180.0,
+            height: 100.0,
+            color: Colors.yellow,
+          ),
+          Container(
+            width: 180.0,
+            height: 100.0,
+            color: Colors.black45,
+          ),
+          Container(
+            width: 180.0,
+            height: 100.0,
+            color: Colors.deepOrange,
+          ),
+          Container(
+            width: 180.0,
+            height: 100.0,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /*
- *图片组件使用 本地图片 远程图片
+ * 顶部导航条，中间内容区域展示demo
  */
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("图片组件 本地 远程 圆角"),
+          title: Text("flutter title"),
         ),
         body: HomeContent(),
       ),
@@ -23,63 +23,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//图片相关属性
+//内容区域
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Image.asset(
-          'images/luckly_button_start.png', //本地图片需要在pubspec.yaml配置所有图片的路径
-          // child: Image.network(
-          //   'https://goss.veer.com/creative/vcg/veer/800water/veer-308713666.jpg',//远程图片的配置
-          color: Colors.red, //图片的背景颜色通常与colorBlendMode混合使用
-          colorBlendMode: BlendMode.screen,
-          alignment: Alignment.bottomRight, //图片的显示位置
-          fit: BoxFit.cover, //cover(充满整个容器，保持不变形) fill（充满屏幕）
-        ),
-        width: 300.0,
-        height: 300.0,
-        decoration: BoxDecoration(color: Colors.red),
-      ),
-    );
-  }
-}
-
-//ClipOval实现圆形图片
-class HomeContent2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: ClipOval(
-          child: Image.network(
-            'https://goss.veer.com/creative/vcg/veer/800water/veer-308713666.jpg',
-            width: 300.0,
-            height: 300.0,
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-//使用BoxDecoration实现圆角图片
-class HomeContent3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300.0,
-        height: 300.0,
-        decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(150),
-            image: DecorationImage(
-                image: NetworkImage(
-                    'https://goss.veer.com/creative/vcg/veer/800water/veer-308713666.jpg'),
-                fit: BoxFit.cover)),
+      child: Text(
+        '你好flutter',
+        textDirection: TextDirection.ltr,
+        style:
+            TextStyle(fontSize: 40.0, color: Color.fromRGBO(2, 243, 122, 1.0)),
       ),
     );
   }
